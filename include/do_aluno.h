668889b05ffdae0376e25e3cb0bfc796521fbcef
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-03 14:08:18 -0300

    Resolvendo o problema

--- include/do_aluno.h
+++ include/do_aluno.h
@@ -2,7 +2,14 @@
 #define DO_ALUNO
 
 #include "constante.h"
+#include "do_professor.h"
 
 int solveMaze(int maze[N][N]);
 
-#endif DO_ALUNO
+int solveMazeAux(int **labirinto, int x, int y, int movX[4], int movY[4]);
+
+int **craeteMaze(int maze[N][N]);
+
+    void freeMaze(int **labirinto);
+
+#endif DO_ALUNOs
--- src/do_aluno.c
+++ src/do_aluno.c
@@ -2,15 +2,77 @@
 
 int solveMaze(int maze[N][N])
 {
-    // Iniciar a busca a partir do ponto de partida (0, 0)
-    /*
-    if (solveMazeUtil(maze, 0, 0) == 0) {
-        printf("Não há solução para o labirinto.\n");
-        return 0;
-    }
-    */
 
-    // Imprimir
+    int movX[] = {0, 1, 0, -1}; // Movimento horizontal (direita, baixo, esquerda, cima)
+    int movY[] = {1, 0, -1, 0}; // Movimento vertical
+
+    int **pmaze = craeteMaze(maze);
+    int resultado = solveMazeAux(pmaze, 0, 0, movX, movY);
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            maze[i][j] = pmaze[i][j];
+            printf("%d", pmaze[i][j]);
+        }
+        printf("\n");
+    }
+
     printMaze(maze);
-    return 1;
+
+    freeMaze(pmaze);
+
+    return resultado;
+}
+
+int solveMazeAux(int** labirinto, int x, int y, int movX[4], int movY[4])
+{
+    if (x < 0 || x >= N || y < 0 || y >= N || labirinto[x][y] == 1 || labirinto[x][y] == 2)
+    {
+        return 0; // Fora dos limites ou parede
+    }
+    if (x == N - 1 && y == N - 1){
+        return 1; // Chegou à posição final
+    }
+
+
+    labirinto[x][y] = 2; // Marca a posição como visitada
+
+    for (int i = 0; i < 4; i++)
+        {
+        if (solveMazeAux(labirinto, x + movX[i], y + movY[i], movX, movY)){
+            return 1; // Encontrou um caminho
+        }
+    }
+
+    labirinto[x][y] = 0;
+    return 0;
+}
+
+int **craeteMaze(int maze[N][N])
+{
+    int **labirinto = malloc(N * sizeof(int *));
+    if (!labirinto)
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        labirinto[i] = calloc(N, sizeof(int));
+
+        for(int j = 0; j < N; j ++){
+            labirinto[i][j] = maze[i][j];
+        }
+    }
+    return labirinto;
+}
+
+// Função auxiliar para desalocar um labirinto alocado com allocateMaze
+void freeMaze(int **labirinto)
+{
+    for (int i = 0; i < N; i++)
+    {
+        free(labirinto[i]);
+    }
+    free(labirinto);
 }
